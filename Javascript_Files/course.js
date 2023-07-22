198c4ff88453b6711cc64ca080d9c48d34d4a759
Vaibhav <codingvaibhav247@gmail.com>
2023-07-22 09:13:53 +0530

    almost work done courses section remaining

---
+++ Javascript_Files/course.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $(".courses-slider2").hide();
+    $(document).on('click', "#btn", function () {
+        var morelessbtn = $(".courses-slider2").is(":visible") ? 'Show More' : 'Show Less';
+        $(this).text(morelessbtn);
+        $(this).parent(".box").find(".courses-slider2").toggle();
+        $(this).parent(".box").find(".courses-slider1").toggle();
+    });
+});
+
+var swiper = new Swiper(".teachers-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      
+    breakpoints: {
+        540: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
---
+++ Javascript_Files/sample.js
@@ -0,0 +1,62 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+var swiper = new Swiper(".courses-slider", {
+  effect: "flip",
+  grabCursor: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".teachers-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
