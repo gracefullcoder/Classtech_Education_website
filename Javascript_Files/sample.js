a47037b5494cf72c2747ae6d7ff3e298a5447bbe
Vaibhav <codingvaibhav247@gmail.com>
2023-08-02 23:39:38 +0530

    completed

--- Javascript_Files/sample.js
+++ Javascript_Files/sample.js
@@ -1,67 +1,62 @@
-// Get the menu button and navigation menu elements
 let menuBtn = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .flex .navbar');
 
-// Toggle the mobile menu when the menu button is clicked
 menuBtn.onclick = () => {
-  menuBtn.classList.toggle('fa-times'); // Toggle the 'fa-times' class on the menu button icon
-  navbar.classList.toggle('active'); // Toggle the 'active' class on the navigation menu
+  menuBtn.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
 }
 
-// Initialize the Swiper slider for the courses section
-var swiperCourses = new Swiper(".courses-slider", {
-  effect: "flip", // Set the transition effect to 'flip'
-  grabCursor: true, // Enable the grab cursor on slider
+var swiper = new Swiper(".courses-slider", {
+  effect: "flip",
+  grabCursor: true,
   autoplay: {
-    delay: 2500, // Set the autoplay delay to 2500ms (2.5 seconds)
-    disableOnInteraction: false, // Disable autoplay when user interacts with slider
+    delay: 2500,
+    disableOnInteraction: false,
   },
   pagination: {
-    el: ".swiper-pagination", // Set the element to display the pagination dots
+    el: ".swiper-pagination",
   },
   navigation: {
-    nextEl: ".swiper-button-next", // Set the element to display the next slide button
-    prevEl: ".swiper-button-prev", // Set the element to display the previous slide button
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-// Initialize the Swiper slider for the teachers section
-var swiperTeachers = new Swiper(".teachers-slider", {
-  spaceBetween: 20, // Set the space between slides to 20px
-  grabCursor: true, // Enable the grab cursor on slider
-  loop: true, // Enable infinite loop of slides
+var swiper = new Swiper(".teachers-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
 
   pagination: {
-    el: ".swiper-pagination", // Set the element to display the pagination dots
-    clickable: true, // Enable clickable pagination dots
+    el: ".swiper-pagination",
+    clickable: true,
   },
   breakpoints: {
     540: {
-      slidesPerView: 1, // Show 1 slide per view on screens with width less than 540px
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2, // Show 2 slides per view on screens with width less than 768px
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3, // Show 3 slides per view on screens with width less than 1024px
+      slidesPerView: 3,
     },
   },
 });
 
-// Initialize the Swiper slider for the reviews section
-var swiperReviews = new Swiper(".reviews-slider", {
-  spaceBetween: 30, // Set the space between slides to 30px
-  centeredSlides: true, // Enable centered slides
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
   autoplay: {
-    delay: 2500, // Set the autoplay delay to 2500ms (2.5 seconds)
-    disableOnInteraction: false, // Disable autoplay when user interacts with slider
+    delay: 2500,
+    disableOnInteraction: false,
   },
   pagination: {
-    el: ".swiper-pagination", // Set the element to display the pagination dots
-    clickable: true, // Enable clickable pagination dots
+    el: ".swiper-pagination",
+    clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next", // Set the element to display the next slide button
-    prevEl: ".swiper-button-prev", // Set the element to display the previous slide button
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
